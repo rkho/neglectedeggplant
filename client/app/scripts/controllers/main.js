e5c2fcb7be0fbf65b0165bc6352e9cef745b851f
Richard Kho <rkho@me.com>
2015-03-12 15:39:45 -0700

    Basic Typeahead searching Google locations in index.html enabled. Styling not yet complete.

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -8,10 +8,17 @@
  * Controller of the testangularApp
  */
 angular.module('wayfareApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $http) {
+    $scope.getLocation = function(val) {
+      return $http.get('http://maps.googleapis.com/maps/api/geocode/json', {
+        params: {
+          address: val,
+          sensor: false
+        }
+      }).then(function(response){
+        return response.data.results.map(function(item){
+          return item.formatted_address;
+        });
+      });
+    };
   });
