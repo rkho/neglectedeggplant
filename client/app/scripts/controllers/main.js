402316f82dfb4f550420d23e5ea8a29133618232
rkho <rkho@me.com>
2015-03-15 22:02:54 -0700

    Front end is fully functional, will send data to server.

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -5,43 +5,39 @@ angular.module('wayfareApp')
   .controller('MainCtrl', function ($scope, $http, myFactory, $modal, $location) {
     // data is an undefined variable until it gets populated on line 9
     $scope.data;
-    $scope.custObj = {};
-    // Get request to 'getAirportData' which is routed in routes.js on the server side, which grabs our Airports.JSON data and assigns the entire object to the $scope.data variable on line 7 above.
+    // Pass on 'myFactory' functions and properties over to the scope of the Main Controller. This grants it access to the 'sendData' function as well as the destination, home, budget, and email properties when needed.
+    $scope.custObj = myFactory;
+    // Get request to 'getAirportData' which is routed in routes.js on the server side, which grabs our Airports.JSON data and assigns the entire object to the $scope.data variable above.
     $http.get('getAirportData')
       .then(function(res){
         $scope.data = res.data;
       })
-    // Submit will take a key/value pair and send it to the factory on line 25.
-    $scope.submit = function(key, value){
-      myFactory.sendData(key, value);
-    };
-    // Four 'getter' functions to access the factory data stored. This is done because Angular controllers reset themselves whenever a view is changed.
-    $scope.custObj.getDestination = myFactory.destination;
-    $scope.custObj.getHome = myFactory.home;
-    $scope.custObj.getBudget = myFactory.budget;
-    $scope.custObj.getEmail = myFactory.email;
-
-
     // Opens a modal
     $scope.open = function(){
       var modalInstance = $modal.open({
         templateUrl: 'app/views/modal.html',
-        controller: 'MainCtrl'
+        controller: 'ModalInstanceCtrl'
       })
     }
-
-    $scope.success = function(){
-      $scope.custObj.getEmail = myFactory.email;
-      $http.post('usertodatabase', $scope.custObj)
+  })
+  .controller('ModalInstanceCtrl', function($scope, $modalInstance, $http, myFactory){
+    $scope.submit = function(key, value){
+      myFactory.sendData(key, value);
     }
+    $scope.custObj = myFactory;
 
-
+    $scope.ok = function(){
+      $modalInstance.close();
+    }
+    $scope.success = function(){
+      $http.post('usertodatabase', myFactory)
+    }
   })
   .factory('myFactory', function(){
     var service = {};
     // This will take in a key/value pair. Key is either 'destination', 'location', or 'budget' based on which view we're collecting information.
     service.sendData = function(key, value){
-      // Line 27 is only necessary if the key is NOT budget or email, because the string passed along to value is a concatenation of airport data's name + airport code, and we only want the airport code in the end.
+      // This line below is only necessary if the key is NOT budget or email, because the string passed along to value is a concatenation of airport data's name + airport code, and we only want the airport code in the end.
       if (key === 'destination' || key === 'home'){
         this[key] = value.substr(-3);
       }
