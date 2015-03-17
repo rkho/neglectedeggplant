5f772dcac439b98750afef7fcecb242294a67cc0
rebekahkwon <rebekah.kwon@gmail.com>
2015-03-17 16:31:26 -0700

    remove cronscript from server, to be uploaded manually

--- server/cronscript.js
+++
@@ -1,6 +0,0 @@
-var routes = require('./routes.js');
-
-// ping api and email users daily at 12:00pm
-request.get('http://neglectedeggplant.azurewebsites.net/getflights', function(req, res) {
-  req.send(200);
-});
